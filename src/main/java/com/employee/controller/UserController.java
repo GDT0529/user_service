b20e76325e6a61430833d45b0c84a44a1be23d5d
Zhen Luo <zhenluofrank@gmail.com>
2024-09-13 01:55:11 -0400

    cross

--- src/main/java/com/employee/controller/UserController.java
+++ src/main/java/com/employee/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:80")
 @RestController
 @RequestMapping("/user")
 public class UserController {
